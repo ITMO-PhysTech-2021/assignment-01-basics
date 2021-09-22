b4b205226e4757c0e430b3a6d34f0380b9c1de0f
amathael <dan.io.oreshnikov@gmail.com>
2021-09-22 11:55:13 +0300

    Added controls block

---
+++ practice/controls/controls.py
@@ -0,0 +1,21 @@
+import math
+
+
+def transpose(a):
+    """Транспозиция матрицы"""
+    pass
+
+
+def read_lines(input, print):
+    """Количество непустых строк во вводе до первой пустой"""
+    pass
+
+
+def min_prime(n):
+    """Минимальный простой делитель числа"""
+    pass
+
+
+def from_hexadecimal(s):
+    """Перевод из шестнадцатеричной системы счисления"""
+    pass
