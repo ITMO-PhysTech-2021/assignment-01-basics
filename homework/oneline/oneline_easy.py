9a7c77ebe498f8806d5c9a63147c9128a7a312d8
amathael <dan.io.oreshnikov@gmail.com>
2021-09-27 21:43:28 +0300

    Added oneline task

---
+++ homework/oneline/oneline_easy.py
@@ -0,0 +1,13 @@
+def square_sum(a):
+    """Сумма квадратов элементов"""
+    pass
+
+
+def print_dividing(a, b, t, print):
+    """Все числа от a до b, делящиеся на t, через пробел"""
+    pass
+
+
+def shuffle_lists(a, b):
+    """Перемешать массивы в порядке чередования"""
+    pass
